225bd3ba97978db8ff5de1ff258eee436b29c0c9
PhillipTrinh <pdtrinh100@student.hbuhsd.edu>
2024-09-04 11:05:53 -0700

    u

--- problem1.java
+++ problem1.java
@@ -1,1 +1,11 @@
 // Create your class here; don't forget the main method
+public class proby1
+{
+ public static void main(String[] args)
+ {
+    String food = "bananan";
+
+    System.out.println(food);
+ }
+ 
+}
--- problem2.java
+++ problem2.java
@@ -1,1 +1,17 @@
 // Create your class here; don't forget the main method
+public class proby2
+{
+ public static void main(String[] args)
+ {
+    String name1 = "Julie";
+
+    String name2 = "Julie2";
+
+    String name3 = "Julie3";
+
+    String name4 = "Julie4";
+
+    System.out.println(name1 + name2 + name3 + name 4);
+ }
+ 
+}
