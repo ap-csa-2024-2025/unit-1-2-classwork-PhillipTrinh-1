9dbb07aea21f1444b08bbd526c60aa5dc44faad6
PhillipTrinh <pdtrinh100@student.hbuhsd.edu>
2024-09-05 10:48:53 -0700

    hhhhhhhh

--- problem3.java
+++ problem3.java
@@ -1,1 +1,13 @@
 // Create your class here; don't forget the main method
+public class proby1
+{
+ public static void main(String[] args)
+ {
+    String name = "Phillip";
+    
+    String home = "Californiaiaiaaiaia"
+
+    System.out.println("Hi My name is " + name + "and I live in " + home);
+ }
+ 
+}
